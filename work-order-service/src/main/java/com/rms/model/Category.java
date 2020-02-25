0b9c894a9b9f515a5cc280d63ec157746a5bf967
ArdonPolukoff <ardonpolukoff@gmail.com>
2020-02-25 09:45:49 -0500

    created category class

---
+++ work-order-service/src/main/java/com/rms/model/Category.java
@@ -0,0 +1,15 @@
+package com.rms.model;
+
+
+
+public enum Category {
+    LIGHTING(1), AIR_CONDITIONING(2), DOORS(3), PROJECTOR(4), OTHER(5);
+
+    private final int type;
+
+    private Category(int type){this.type = type;}
+
+    public int getValue() {
+        return type;
+    }
+}
