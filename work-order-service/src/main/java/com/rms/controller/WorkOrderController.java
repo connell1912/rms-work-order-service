695d2c13c783abc82e9bae57fe7e67cb487ba2bf
Connor Casey <connorcaseyc@gmail.com>
2020-02-25 15:17:35 -0500

    fixing controller

--- work-order-service/src/main/java/com/rms/controller/WorkOrderController.java
+++ work-order-service/src/main/java/com/rms/controller/WorkOrderController.java
@@ -1,5 +1,54 @@
 package com.rms.controller;
 
+import com.rms.dao.WorkOrderDao;
+import com.rms.model.WorkOrder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/workorder")
 public class WorkOrderController {
+
+    @Autowired
+    WorkOrderDao wod; 
+
+    @GetMapping("all")
+    public Iterable<WorkOrder> findAll() {
+        return wod.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public WorkOrder findById(@PathVariable("id") int id) {
+        return wod.findById(id).get();
+    }
+
+    @PostMapping
+    public String insert(@RequestBody WorkOrder wo) {
+        wod.save(wo);
+        return "Saved Work Order";
+    } 
+
+    @PutMapping("/{id}")
+    public String updateResolved(@PathVariable("id") int id, String resolvedtime) {
+        WorkOrder wo = wod.findById(id).get();
+        wo.setResolvedDateTime(resolvedtime);
+        wod.save(wo);
+        return "Work Order Updated";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        wod.deleteById(id);
+        return "Work Order Deleted";
+    }
+
     
 }
--- work-order-service/src/main/java/com/rms/service/WorkOrderService.java
+++ work-order-service/src/main/java/com/rms/service/WorkOrderService.java
@@ -1,47 +1,9 @@
 package com.rms.service;
 
-import com.rms.dao.WorkOrderDao;
-import com.rms.model.WorkOrder;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class WorkOrderService {
-    
-    @Autowired
-    WorkOrderDao wod; 
-
-    @GetMapping("/{id}")
-    public WorkOrder findById(@PathVariable("id") int id) {
-        return wod.findById(id).get();
-    }
-
-    @PostMapping
-    public String insert(@RequestBody WorkOrder wo) {
-        wod.save(wo);
-        return "Saved Work Order";
-    } 
-
-    @PutMapping("/{id}")
-    public String updateResolved(@PathVariable("id") int id, String resolvedtime) {
-        WorkOrder wo = wod.findById(id).get();
-        wo.setResolvedDateTime(resolvedtime);
-        wod.save(wo);
-        return "Work Order Updated";
-    }
-
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id) {
-        wod.deleteById(id);
-        return "Work Order Deleted";
-    }
 
 
 
