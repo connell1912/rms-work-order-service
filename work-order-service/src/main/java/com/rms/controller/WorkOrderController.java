1e5b67031bf0b6bff631a33bbe6609a8fb9dad7b
Connor Casey <connorcaseyc@gmail.com>
2020-02-27 18:22:39 -0500

    react app

--- work-order-service/src/main/java/com/rms/controller/WorkOrderController.java
+++ work-order-service/src/main/java/com/rms/controller/WorkOrderController.java
@@ -4,8 +4,10 @@ import java.sql.Timestamp;
 
 import com.rms.dao.WorkOrderDao;
 import com.rms.model.WorkOrder;
+import com.rms.service.WorkOrderService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,38 +21,39 @@ import ch.qos.logback.core.joran.action.TimestampAction;
 
 @RestController
 @RequestMapping("/workorder")
+@CrossOrigin(origins={"http://localhost:3000"})
 public class WorkOrderController {
 
     @Autowired
-    WorkOrderDao wod; 
+    WorkOrderService wos; 
 
-    @GetMapping("all")
+    @GetMapping("/all")
     public Iterable<WorkOrder> findAll() {
-        return wod.findAll();
+        return wos.grabAll();
     }
 
     @GetMapping("/{id}")
     public WorkOrder findById(@PathVariable("id") int id) {
-        return wod.findById(id).get();
+        return wos.grabById(id);
     }
 
-    @PostMapping
+    @PostMapping("/newworkorder")
     public String insert(@RequestBody WorkOrder wo) {
-        wod.save(wo);
+        wos.addWorkOrder(wo);
         return "Saved Work Order";
     } 
 
-    @PutMapping("/{id}")
-    public String updateResolved(@PathVariable("id") int id, Timestamp resolvedtime) {
-        WorkOrder wo = wod.findById(id).get();
-        wo.setResolvedDateTime(resolvedtime);
-        wod.save(wo);
-        return "Work Order Updated";
-    }
+    // @PutMapping("/{id}")
+    // public String updateResolved(@PathVariable("id") int id, Timestamp resolvedtime) {
+    //     WorkOrder wo = wod.findById(id).get();
+    //     wo.setResolvedDateTime(resolvedtime);
+    //     wod.save(wo);
+    //     return "Work Order Updated";
+    // }
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") int id) {
-        wod.deleteById(id);
+        wos.deleteWorkOrder(id);
         return "Work Order Deleted";
     }
 
