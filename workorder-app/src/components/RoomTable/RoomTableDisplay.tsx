f28789524541e9c27302dc50376f4f754dd42d68
{connorcaseyc} <{connorcaseyc@gmail.com}>
2020-03-03 14:48:04 -0500

    yep

--- workorder-app/src/components/RoomTable/RoomTableDisplay.tsx
+++ workorder-app/src/components/RoomTable/RoomTableDisplay.tsx
@@ -12,7 +12,7 @@ export const RoomTableDisplay:React.FC<IRR> = (props:IRR) => {
         <>
             <tr key={props.room.id}>
                 <td key={props.room.id}>{props.room.id}</td>
-                <td key={props.room.currentStatus}>{props.room.currentStatus}</td>
+                {/* <td key={props.room.currentStatus}>{props.room.currentStatus}</td> */} 
                 <td key={props.room.roomNumber}>{props.room.roomNumber}</td>
                 <td>{props.room.isActive}</td>
                 <td key={props.room.maxOccupancy}>{props.room.maxOccupancy}</td>
