5c1891305467ac03d791c9ba9a0ae086256fad32
connorcaseyc <58892902+connorcaseyc@users.noreply.github.com>
2020-03-10 21:58:14 -0400

    Update WorkOrderServiceApplication.java

--- work-order-service/src/main/java/com/rms/WorkOrderServiceApplication.java
+++ work-order-service/src/main/java/com/rms/WorkOrderServiceApplication.java
@@ -1,15 +1,8 @@
 package com.rms;
 
-import com.rms.dao.WorkOrderDao;
-import com.rms.model.Category;
-import com.rms.model.Status;
-import com.rms.model.WorkOrder;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
